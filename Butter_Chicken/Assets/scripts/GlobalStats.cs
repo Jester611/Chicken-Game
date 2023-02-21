f2bcdb99e0f8963b00c683f6701b27b6cc3d0f5f
Jester611 <pittyconor@gmail.com>
2023-02-21 20:16:58 +0100

    Globalstats inspector
    Globalstats inspector categorized

--- Butter_Chicken/Assets/scripts/GlobalStats.cs
+++ Butter_Chicken/Assets/scripts/GlobalStats.cs
@@ -10,20 +10,24 @@ public class GlobalStats : MonoBehaviour
     //this is a terrible solution and is probably better reworked
 
     // ## VARIABLES RELATED TO UPGRADES ##
+    [Header("Bullet")]
     public float bulletDamage;
     public float bulletSize;
     public float bulletExplosiveRadius;
     public float bulletKnockback;
+    [Header("Weapon")]
     public float gunRateOfFire;
     public float gunBulletSpeed;
+    [Header("Player")]
     public float playerSpeed;
+    public int playerLevelRequirement;
+    [Header("Enemy")]
     public float enemyHP;
     public float enemySpeed;
     public float enemySize;
 
     // Player stats
     private int playerXP;
-    public int playerLevelRequirement;
 
     private void Awake() {
         if (instance == null) {
