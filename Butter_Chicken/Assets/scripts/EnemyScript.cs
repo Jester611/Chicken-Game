6fe0c92bdbd1b5dc0710b5782c1b67e137f14470
carpix26 <125826796+carpix26@users.noreply.github.com>
2023-02-21 14:48:22 +0100

    Update EnemyScript.cs

--- Butter_Chicken/Assets/scripts/EnemyScript.cs
+++ Butter_Chicken/Assets/scripts/EnemyScript.cs
@@ -3,23 +3,29 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+
+    public int enemykill;
+
     [SerializeField] Transform player;
-    [SerializeField] Rigidbody rb;
     float maxHP;
     float movementSpeed;
+    [SerializeField] Rigidbody rb;
     private float currentHP;
     private float distance;
     Vector3 direction;
 
     public static event Action OnEnemyKilled;
+    
+   
 
+    
     private void Awake() {
-        movementSpeed = GlobalStats.instance.enemySpeed;
-        maxHP = GlobalStats.instance.enemyHP;
+        movementSpeed = GlobalStats._instance.enemySpeed;
+        maxHP = GlobalStats._instance.enemyHP;
         currentHP = maxHP;
     }
 
-    public void TakeDamage(float damageTaken) {
+    public void TakeDamage(float damageTaken){
         currentHP -= damageTaken;
     }
     private void OnEnable() {
@@ -27,32 +33,43 @@ public class EnemyScript : MonoBehaviour
     }
     private void OnDisable() {
         LevelUpScript.OnUpdateStats -= UpdateStats;
+        
     }
 
-    void Update() {
-        if (currentHP <= 0) {
+    void Update()
+    {
+    
+    if (currentHP <= 0){
             Die();
+           
         }
+
+    if(currentHP <= 0)
+    {
+        enemykill = enemykill +1;
+    }
     }
 
-    private void FixedUpdate() {
+
+
+    private void FixedUpdate(){
+
         distance = Vector3.Distance(transform.position, player.position);
         direction = player.position - transform.position;
         direction.Normalize();
-
-        rb.AddForce(direction * movementSpeed, ForceMode.VelocityChange);
+        
+        rb.AddForce(direction*movementSpeed, ForceMode.VelocityChange);
 
         float angle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
     }
-
-    private void Die() {
+    private void Die(){
         Destroy(gameObject);
         OnEnemyKilled?.Invoke();
     }
 
-    private void UpdateStats() {
-        movementSpeed = GlobalStats.instance.enemySpeed;
-        maxHP = GlobalStats.instance.enemyHP;
+    private void UpdateStats(){
+        movementSpeed = GlobalStats._instance.enemySpeed;
+        maxHP = GlobalStats._instance.enemyHP;
     }
 }
